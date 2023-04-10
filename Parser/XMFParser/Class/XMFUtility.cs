7cae419bb118bff91fca178eaa8ec7a2847359ce
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2023-04-10 21:57:43 +0700

    Use CheckIfXMFVersionMatches() to return (bool, int[]) tuple
    Also adding XMFVersionLength

--- Parser/XMFParser/Class/XMFUtility.cs
+++ Parser/XMFParser/Class/XMFUtility.cs
@@ -8,6 +8,11 @@ namespace Hi3Helper.EncTool.Parser.AssetMetadata
     public static class XMFUtility
     {
         /// <summary>
+        /// Gets the length of the version that can be received by the XMFParser
+        /// </summary>
+        public static int XMFVersionLength { get => XMFParser._versioningLength; }
+
+        /// <summary>
         /// Compares the version given from <c>versionBytes</c> with the one from xmfPath.
         /// </summary>
         /// <param name="xmfPath">The path of the XMF file to compare.</param>
@@ -17,12 +22,12 @@ namespace Hi3Helper.EncTool.Parser.AssetMetadata
         /// If the version contained in the XMF file matches the one from <c>versionBytes</c> or there's any fault, then return <c>false</c>.<br/>
         /// If it matches, then return <c>true</c>.
         /// </returns>
-        public static bool CheckIfXMFVersionMatches(string xmfPath, ReadOnlySpan<int> versionBytes, bool use4LengthArrayCompare = false)
+        public static (bool, int[]) CheckIfXMFVersionMatches(string xmfPath, ReadOnlySpan<int> versionBytes, bool use4LengthArrayCompare = false)
         {
             FileInfo xmf = new FileInfo(xmfPath);
 
-            if (!xmf.Exists || (xmf.Exists && xmf.Length < 0xFF)) return false;
-            if (versionBytes.Length != XMFParser._versioningLength) return false;
+            if (!xmf.Exists || (xmf.Exists && xmf.Length < 0xFF)) return (false, new int[] { });
+            if (versionBytes.Length != XMFParser._versioningLength) return (false, new int[] { });
 
             try
             {
@@ -32,13 +37,14 @@ namespace Hi3Helper.EncTool.Parser.AssetMetadata
                     reader.Position = XMFParser._signatureLength + 4;
                     ReadOnlySpan<int> versionXMF = XMFParser.ReadVersion(reader);
 
-                    return versionXMF[0] == versionBytes[0] && versionXMF[1] == versionBytes[1] && versionXMF[2] == versionBytes[2] && (use4LengthArrayCompare ? versionXMF[3] == versionBytes[3] : true);
+                    return (versionXMF[0] == versionBytes[0] && versionXMF[1] == versionBytes[1] && versionXMF[2] == versionBytes[2] && (use4LengthArrayCompare ? versionXMF[3] == versionBytes[3] : true),
+                            versionXMF.ToArray());
                 }
             }
             catch (Exception e)
             {
                 Console.WriteLine($"error while reading XMF file!\r\n{e}");
-                return false;
+                return (false, new int[] { });
             }
         }
     }
