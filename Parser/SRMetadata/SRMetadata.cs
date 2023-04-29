886334fb3893808946f87da262d50b055661e0e2
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2023-04-30 00:51:04 +0700

    Add cached initialize mechanism

--- Parser/SRMetadata/SRMetadata.cs
+++ Parser/SRMetadata/SRMetadata.cs
@@ -14,6 +14,7 @@ namespace Hi3Helper.EncTool.Parser.AssetMetadata
 
         private SRDispatcherInfo _dispatcherInfo { get; init; }
         private Http.Http _httpClient { get; set; }
+        private bool _isInitialized { get; set; }
 
         public SRMetadataBase MetadataIFix { get; set; }
         public SRMetadataBase MetadataDesign { get; set; }
@@ -27,6 +28,7 @@ namespace Hi3Helper.EncTool.Parser.AssetMetadata
         {
             _httpClient = new Http.Http(true, 5, 1000, _userAgent);
             _dispatcherInfo = new SRDispatcherInfo(_httpClient, dispatchURL, dispatchSeed, dispatchFormatTemplate, gatewayFormatTemplate, productID, productVer);
+            _isInitialized = false;
         }
 
         public void Dispose()
@@ -47,90 +49,146 @@ namespace Hi3Helper.EncTool.Parser.AssetMetadata
             MetadataLua = null;
             MetadataAudio = null;
             MetadataVideo = null;
+
+            _isInitialized = false;
         }
 
-        public async Task Initialize(CancellationToken threadToken, byte regionID)
+        public async Task Initialize(CancellationToken threadToken, byte regionID, bool forceInitialize = false)
         {
-            _httpClient.DownloadProgress += HttpProgressAdapter;
-            await _dispatcherInfo.Initialize(threadToken, regionID);
-            _httpClient.DownloadProgress -= HttpProgressAdapter;
+            if (!_isInitialized || forceInitialize)
+            {
+                try
+                {
+                    _httpClient.DownloadProgress += HttpProgressAdapter;
+                    await _dispatcherInfo.Initialize(threadToken, regionID);
+
+                    _isInitialized = true;
+                }
+                catch { throw; }
+                finally
+                {
+                    _httpClient.DownloadProgress -= HttpProgressAdapter;
+                }
+            }
         }
 
         public async Task ReadIFixMetadataInformation(CancellationToken threadToken)
         {
-            _httpClient.DownloadProgress += HttpProgressAdapter;
+            try
+            {
+                _httpClient.DownloadProgress += HttpProgressAdapter;
 
-            MetadataIFix = SRIFixMetadata.CreateInstance(_dispatcherInfo._regionGateway.IFixPatchVersionUpdateUrl, _httpClient);
-            await MetadataIFix.GetRemoteMetadata(threadToken);
-            MetadataIFix.Deserialize();
-
-            _httpClient.DownloadProgress -= HttpProgressAdapter;
+                MetadataIFix = SRIFixMetadata.CreateInstance(_dispatcherInfo._regionGateway.IFixPatchVersionUpdateUrl, _httpClient);
+                await MetadataIFix.GetRemoteMetadata(threadToken);
+                MetadataIFix.Deserialize();
+            }
+            catch { throw; }
+            finally
+            {
+                _httpClient.DownloadProgress -= HttpProgressAdapter;
+            }
         }
 
         public async Task ReadDesignMetadataInformation(CancellationToken threadToken)
         {
-            _httpClient.DownloadProgress += HttpProgressAdapter;
+            try
+            {
+                _httpClient.DownloadProgress += HttpProgressAdapter;
 
-            MetadataDesign = SRDesignMetadata.CreateInstance(_dispatcherInfo._regionGateway.DesignDataBundleVersionUpdateUrl, _httpClient);
-            await MetadataDesign.GetRemoteMetadata(threadToken);
-            MetadataDesign.Deserialize();
-
-            _httpClient.DownloadProgress -= HttpProgressAdapter;
+                MetadataDesign = SRDesignMetadata.CreateInstance(_dispatcherInfo._regionGateway.DesignDataBundleVersionUpdateUrl, _httpClient);
+                await MetadataDesign.GetRemoteMetadata(threadToken);
+                MetadataDesign.Deserialize();
+            }
+            catch { throw; }
+            finally
+            {
+                _httpClient.DownloadProgress -= HttpProgressAdapter;
+            }
         }
 
         public async Task ReadLuaMetadataInformation(CancellationToken threadToken)
         {
-            _httpClient.DownloadProgress += HttpProgressAdapter;
+            try
+            {
+                _httpClient.DownloadProgress += HttpProgressAdapter;
 
-            MetadataLua = SRLuaMetadata.CreateInstance(_dispatcherInfo._regionGateway.LuaBundleVersionUpdateUrl, _httpClient);
-            await MetadataLua.GetRemoteMetadata(threadToken);
-            MetadataLua.Deserialize();
-
-            _httpClient.DownloadProgress -= HttpProgressAdapter;
+                MetadataLua = SRLuaMetadata.CreateInstance(_dispatcherInfo._regionGateway.LuaBundleVersionUpdateUrl, _httpClient);
+                await MetadataLua.GetRemoteMetadata(threadToken);
+                MetadataLua.Deserialize();
+            }
+            catch { throw; }
+            finally
+            {
+                _httpClient.DownloadProgress -= HttpProgressAdapter;
+            }
         }
 
         public async Task ReadAsbMetadataInformation(CancellationToken threadToken)
         {
-            _httpClient.DownloadProgress += HttpProgressAdapter;
+            try
+            {
+                _httpClient.DownloadProgress += HttpProgressAdapter;
 
-            MetadataAsb = SRAsbMetadata.CreateInstance(_dispatcherInfo.ArchiveInfo, _dispatcherInfo._regionGateway.AssetBundleVersionUpdateUrl, _httpClient);
-            await MetadataAsb.GetRemoteMetadata(threadToken);
-            MetadataAsb.Deserialize();
-
-            _httpClient.DownloadProgress -= HttpProgressAdapter;
+                MetadataAsb = SRAsbMetadata.CreateInstance(_dispatcherInfo.ArchiveInfo, _dispatcherInfo._regionGateway.AssetBundleVersionUpdateUrl, _httpClient);
+                await MetadataAsb.GetRemoteMetadata(threadToken);
+                MetadataAsb.Deserialize();
+            }
+            catch { throw; }
+            finally
+            {
+                _httpClient.DownloadProgress -= HttpProgressAdapter;
+            }
         }
 
         public async Task ReadBlockMetadataInformation(CancellationToken threadToken)
         {
-            _httpClient.DownloadProgress += HttpProgressAdapter;
+            try
+            {
+                _httpClient.DownloadProgress += HttpProgressAdapter;
 
-            MetadataBlock = SRBlockMetadata.CreateInstance(_dispatcherInfo.ArchiveInfo, _dispatcherInfo._regionGateway.AssetBundleVersionUpdateUrl, _httpClient);
-            await MetadataBlock.GetRemoteMetadata(threadToken);
-            MetadataBlock.Deserialize();
-
-            _httpClient.DownloadProgress -= HttpProgressAdapter;
+                MetadataBlock = SRBlockMetadata.CreateInstance(_dispatcherInfo.ArchiveInfo, _dispatcherInfo._regionGateway.AssetBundleVersionUpdateUrl, _httpClient);
+                await MetadataBlock.GetRemoteMetadata(threadToken);
+                MetadataBlock.Deserialize();
+            }
+            catch { throw; }
+            finally
+            {
+                _httpClient.DownloadProgress -= HttpProgressAdapter;
+            }
         }
 
         public async Task ReadAudioMetadataInformation(CancellationToken threadToken)
         {
-            _httpClient.DownloadProgress += HttpProgressAdapter;
+            try
+            {
+                _httpClient.DownloadProgress += HttpProgressAdapter;
 
-            MetadataAudio = SRAudioMetadata.CreateInstance(_dispatcherInfo.ArchiveInfo, _dispatcherInfo._regionGateway.AssetBundleVersionUpdateUrl, _httpClient);
-            await MetadataAudio.GetRemoteMetadata(threadToken);
-            MetadataAudio.Deserialize();
-
-            _httpClient.DownloadProgress -= HttpProgressAdapter;
+                MetadataAudio = SRAudioMetadata.CreateInstance(_dispatcherInfo.ArchiveInfo, _dispatcherInfo._regionGateway.AssetBundleVersionUpdateUrl, _httpClient);
+                await MetadataAudio.GetRemoteMetadata(threadToken);
+                MetadataAudio.Deserialize();
+            }
+            catch { throw; }
+            finally
+            {
+                _httpClient.DownloadProgress -= HttpProgressAdapter;
+            }
         }
 
         public async Task ReadVideoMetadataInformation(CancellationToken threadToken)
         {
-            _httpClient.DownloadProgress += HttpProgressAdapter;
+            try
+            {
+                _httpClient.DownloadProgress += HttpProgressAdapter;
 
-            MetadataVideo = SRVideoMetadata.CreateInstance(_dispatcherInfo.ArchiveInfo, _dispatcherInfo._regionGateway.AssetBundleVersionUpdateUrl, _httpClient);
-            await MetadataVideo.GetRemoteMetadata(threadToken);
-            MetadataVideo.Deserialize();
-
-            _httpClient.DownloadProgress -= HttpProgressAdapter;
+                MetadataVideo = SRVideoMetadata.CreateInstance(_dispatcherInfo.ArchiveInfo, _dispatcherInfo._regionGateway.AssetBundleVersionUpdateUrl, _httpClient);
+                await MetadataVideo.GetRemoteMetadata(threadToken);
+                MetadataVideo.Deserialize();
+            }
+            catch { throw; }
+            finally
+            {
+                _httpClient.DownloadProgress -= HttpProgressAdapter;
+            }
         }
 
         private void HttpProgressAdapter(object sender, DownloadEvent e) => HttpEvent?.Invoke(this, e);
