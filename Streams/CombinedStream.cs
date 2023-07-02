6941b57e40760a663d9a8dce8fcd7cdcab253f74
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2023-07-02 13:37:46 +0700

    Adding write functionality in CombinedStream

--- Streams/CombinedStream.cs
+++ Streams/CombinedStream.cs
@@ -5,9 +5,16 @@
 
 using System;
 using System.IO;
+using System.Linq;
 
 namespace Hi3Helper.EncTool
 {
+    public struct NewFileCombinedStreamStruct
+    {
+        public FileStream stream;
+        public long size;
+    }
+
     /// <summary>
     /// This class is a <see cref="Stream"/> descendant that manages multiple underlying
     /// streams which are considered to be chained together to one large stream. Only reading
@@ -59,6 +66,53 @@ namespace Hi3Helper.EncTool
         }
 
         /// <summary>
+        /// Constructs a new <see cref="CombinedStream"/> on top of the specified array
+        /// of streams.
+        /// </summary>
+        /// <param name="underlyingStreams">
+        /// An array of <see cref="Stream"/> objects that will be chained together and
+        /// considered to be one big stream.
+        /// </param>
+        public CombinedStream(params NewFileCombinedStreamStruct[] underlyingStreams)
+        {
+            if (underlyingStreams == null)
+                throw new ArgumentNullException("underlyingStreams");
+
+            _UnderlyingStreams = new Stream[underlyingStreams.Length];
+            _UnderlyingStartingPositions = new long[underlyingStreams.Length];
+
+            foreach (NewFileCombinedStreamStruct stream in underlyingStreams)
+            {
+                if (stream.stream == null)
+                    throw new ArgumentException("underlyingStreams contains a null stream reference", "underlyingStreams");
+                if (!stream.stream.CanRead)
+                    throw new InvalidOperationException("CanRead not true for all streams");
+                if (!stream.stream.CanSeek)
+                    throw new InvalidOperationException("CanSeek not true for all streams");
+
+                stream.stream.SetLength(stream.size);
+#if DEBUG && SHOWDEBUGINFO
+                Console.WriteLine($"[CombinedStream.ctor()] Initializing file with length {stream.size} bytes: {stream.stream.Name}");
+#endif
+            }
+
+            Array.Copy(underlyingStreams.Select(x => x.stream).ToArray(), _UnderlyingStreams, underlyingStreams.Length);
+
+            _Position = 0;
+            _Index = 0;
+
+            _UnderlyingStartingPositions[0] = 0;
+            for (int index = 1; index < _UnderlyingStartingPositions.Length; index++)
+                _UnderlyingStartingPositions[index] = _UnderlyingStartingPositions[index - 1] + underlyingStreams[index - 1].size;
+
+            _TotalLength = _UnderlyingStartingPositions[_UnderlyingStartingPositions.Length - 1] + underlyingStreams[_UnderlyingStreams.Length - 1].size;
+
+#if DEBUG && SHOWDEBUGINFO
+            Console.WriteLine($"[CombinedStream.ctor()] Total length of the CombinedStream: {_TotalLength} bytes with total of {underlyingStreams.Length} streams");
+#endif
+        }
+
+        /// <summary>
         /// Gets a value indicating whether the current stream supports reading.
         /// </summary>
         /// <value>
@@ -105,7 +159,7 @@ namespace Hi3Helper.EncTool
         {
             get
             {
-                return false;
+                return true;
             }
         }
 
@@ -182,6 +236,38 @@ namespace Hi3Helper.EncTool
             }
         }
 
+        public override int Read(Span<byte> buffer)
+        {
+            int result = 0;
+            int count = buffer.Length;
+            int offset = 0;
+
+            while (count > 0)
+            {
+                _UnderlyingStreams[_Index].Position = _Position - _UnderlyingStartingPositions[_Index];
+                int bytesRead = _UnderlyingStreams[_Index].Read(buffer.Slice(offset));
+                result += bytesRead;
+                offset += bytesRead;
+                count -= bytesRead;
+                _Position += bytesRead;
+
+                if (count > 0)
+                {
+                    if (_Index < _UnderlyingStreams.Length - 1)
+                    {
+                        _Index++;
+#if DEBUG && SHOWDEBUGINFO
+                        Console.WriteLine($"[CombinedStream.Read()] Moving the stream to Index: {_Index}");
+#endif
+                    }
+                    else
+                        break;
+                }
+            }
+
+            return result;
+        }
+
         /// <summary>
         /// Reads a sequence of bytes from the current stream and advances the position within the stream by the number of bytes read.
         /// </summary>
@@ -212,7 +298,12 @@ namespace Hi3Helper.EncTool
                 if (count > 0)
                 {
                     if (_Index < _UnderlyingStreams.Length - 1)
+                    {
                         _Index++;
+#if DEBUG && SHOWDEBUGINFO
+                        Console.WriteLine($"[CombinedStream.Read()] Moving the stream to Index: {_Index}");
+#endif
+                    }
                     else
                         break;
                 }
@@ -265,6 +356,39 @@ namespace Hi3Helper.EncTool
             throw new NotSupportedException("The method or operation is not supported by CombinedStream.");
         }
 
+        public override void Write(ReadOnlySpan<byte> buffer)
+        {
+            int count = buffer.Length;
+            int offset = 0;
+            while (count > 0)
+            {
+                _UnderlyingStreams[_Index].Position = _Position - _UnderlyingStartingPositions[_Index];
+                int bytesWrite = count;
+                int remainedMaxLength = (int)(_UnderlyingStreams[_Index].Length - _UnderlyingStreams[_Index].Position);
+                if (remainedMaxLength < count)
+                {
+                    bytesWrite = remainedMaxLength;
+                }
+                _UnderlyingStreams[_Index].Write(buffer.Slice(offset, bytesWrite));
+                offset += bytesWrite;
+                count -= bytesWrite;
+                _Position += bytesWrite;
+
+                if (count > 0)
+                {
+                    if (_Index < _UnderlyingStreams.Length - 1)
+                    {
+                        _Index++;
+#if DEBUG && SHOWDEBUGINFO
+                        Console.WriteLine($"[CombinedStream.Write()] Moving the stream to Index: {_Index}");
+#endif
+                    }
+                    else
+                        break;
+                }
+            }
+        }
+
         /// <summary>
         /// Throws <see cref="NotSupportedException"/> since the <see cref="CombinedStream"/>
         /// class does not supports writing to the underlying streams.
@@ -277,7 +401,33 @@ namespace Hi3Helper.EncTool
         /// </exception>
         public override void Write(byte[] buffer, int offset, int count)
         {
-            throw new NotSupportedException("The method or operation is not supported by CombinedStream.");
+            while (count > 0)
+            {
+                _UnderlyingStreams[_Index].Position = _Position - _UnderlyingStartingPositions[_Index];
+                int bytesWrite = count;
+                int remainedMaxLength = (int)(_UnderlyingStreams[_Index].Length - _UnderlyingStreams[_Index].Position);
+                if (remainedMaxLength < count)
+                {
+                    bytesWrite = remainedMaxLength;
+                }
+                _UnderlyingStreams[_Index].Write(buffer, offset, bytesWrite);
+                offset += bytesWrite;
+                count -= bytesWrite;
+                _Position += bytesWrite;
+
+                if (count > 0)
+                {
+                    if (_Index < _UnderlyingStreams.Length - 1)
+                    {
+                        _Index++;
+#if DEBUG && SHOWDEBUGINFO
+                        Console.WriteLine($"[CombinedStream.Write()] Moving the stream to Index: {_Index}");
+#endif
+                    }
+                    else
+                        break;
+                }
+            }
         }
     }
 }
