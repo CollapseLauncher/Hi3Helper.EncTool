b352fd20eba15decfc46bd991024d43044255187
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2024-01-20 02:48:04 +0700

    Add byte-to-byte struct comparison with ``IsStructEqual()``

--- Misc/TypeExtensions.cs
+++ Misc/TypeExtensions.cs
@@ -1,12 +1,47 @@
 ﻿using System;
 using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace Hi3Helper.EncTool
 {
     public static class TypeExtensions
     {
+        public static bool IsStructEqual<TStruct>(TStruct self, TStruct to)
+            where TStruct : struct => IsStructEqualFromTo(self, to);
+
+        public unsafe static bool IsStructEqualFromTo<TFrom, TTo>(TFrom self, TTo to)
+            where TFrom : struct
+            where TTo : struct
+        {
+            // Check if the object is equal based on reference or struct can be compared
+            // as a raw data (does not contain any reference type)
+            if (self.Equals(to)) return true;
+
+            // If not, then fallback to check using marshalling
+            // Get the size of the struct
+            int sizeOfTFrom = Marshal.SizeOf<TFrom>();
+            int sizeOfTTo = Marshal.SizeOf<TTo>();
+
+            // Allocate the HGlobal buffer pointer
+            IntPtr bufferPtr = Marshal.AllocHGlobal(sizeOfTFrom + sizeOfTTo); // Allocate size for from and to
+            // Marshal struct into the pointer
+            Marshal.StructureToPtr(self, bufferPtr, true);
+            Marshal.StructureToPtr(to, bufferPtr + sizeOfTFrom, true);
+
+            // Assign as a span and compare the buffer
+            ReadOnlySpan<byte> spanSelf = new ReadOnlySpan<byte>((byte*)bufferPtr, sizeOfTFrom);
+            ReadOnlySpan<byte> spanTo = new ReadOnlySpan<byte>((byte*)bufferPtr + sizeOfTFrom, sizeOfTTo);
+            bool isRawDataEqual = spanSelf.SequenceEqual(spanTo);
+
+            // Free the HGlobal buffer pointer
+            Marshal.FreeHGlobal(bufferPtr);
+
+            // Return the comparison value
+            return isRawDataEqual;
+        }
+
         public static bool IsInstancePropertyEqual<T>(T self, T to)
-        {   
+        {
             // Check if the one of the value is null, if true check the other value if it's null
             if (self == null)
             {
