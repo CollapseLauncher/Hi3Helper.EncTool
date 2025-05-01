97a690473958ae83354132ecd9ebd3028be05470
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2025-05-01 17:33:14 +0700

    Fix missing property name on Paimon's Content

--- Parser/YSDispatchHelper/DispatchInfo.cs
+++ Parser/YSDispatchHelper/DispatchInfo.cs
@@ -1,8 +1,11 @@
 ﻿#nullable enable
+using System.Text.Json.Serialization;
+
 namespace Hi3Helper.EncTool.Parser.YSDispatchHelper
 {
     public class DispatchInfo
     {
+        [JsonPropertyName("content")]
         public string? Content { get; set; }
     }
 }
